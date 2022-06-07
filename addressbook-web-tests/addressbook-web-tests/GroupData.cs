6ea4b812194a3d6c9b52216b69c13219a46b9ee8
arrell42 <arrell42@gmail.com>
2022-06-07 08:29:39 +0700

    Промежуточный коммит

---
+++ addressbook-web-tests/addressbook-web-tests/GroupData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressBookTests
+{
+    internal class GroupData
+    {
+        private string name;
+        private string header = "";
+        private string footer = "";
+                      
+
+        public string Name { get { return name; } set { name = value; } }
+        
+        public string Header { get { return header; } set { header = value; } }
+
+        public string Footer { get { return footer; } set { footer = value; } }
+    }
+}
+
--- addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -7,8 +7,9 @@ namespace addressbook_web_tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodSquare()
         {
         }
+
     }
 }
