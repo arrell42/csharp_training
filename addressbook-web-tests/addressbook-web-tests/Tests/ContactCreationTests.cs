ef7422b793395f3c6c0e73afdd7c2b2e1936407a
Alcogolizm <arrell42@gmail.com>
2022-07-25 17:41:05 +0700

    Лекция 6.1 - добавил для групп и контактов

--- addressbook-web-tests/addressbook-web-tests/Tests/ContactCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/Tests/ContactCreationTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -26,7 +27,19 @@ namespace WebAddressBookTests
             return contacts;
         }
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+        public static IEnumerable<ContactData> ContactDataFromFile()
+        {
+            List<ContactData> contacts = new List<ContactData>();
+            string[] lines = File.ReadAllLines(@"contacts.csv");
+            foreach (string l in lines)
+            {
+                string[] parts = l.Split(',');
+                contacts.Add(new ContactData(parts[0], parts[1]));
+            }
+            return contacts;
+        }
+
+        [Test, TestCaseSource("ContactDataFromFile")]
         public void ContactCreationTest(ContactData contact)
         {   
             // создаем список
